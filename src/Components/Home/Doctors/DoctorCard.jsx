17ed4524bcf4d4526639d72c69fd42efaed0c027
Md Robin Rana <developer.smrobin45@gmail.com>
2023-10-28 19:24:08 +0600

    service edited

---
+++ src/Components/Home/Doctors/DoctorCard.jsx
@@ -0,0 +1,9 @@
+const DoctorCard = () => {
+   return (
+      <div>
+         <h2>D</h2>
+      </div>
+   );
+};
+
+export default DoctorCard;
---
+++ src/Components/Home/Doctors/Doctors.jsx
@@ -0,0 +1,9 @@
+const Doctors = () => {
+   return (
+      <div>
+         <h2>DOctors</h2>
+      </div>
+   );
+};
+
+export default Doctors;
