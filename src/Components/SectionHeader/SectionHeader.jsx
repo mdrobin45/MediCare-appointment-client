eb954e915cdd2400c37753734610d4a053dbb27b
Md Robin Rana <developer.smrobin45@gmail.com>
2023-10-28 14:48:56 +0600

    service edit

--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -1,7 +1,12 @@
+import { BiPlusMedical } from "react-icons/bi";
+
 const SectionHeader = ({ title, subtitle }) => {
    return (
-      <div className="text-center py-6">
-         <h3 className="text-md md:text-xl text-center text-secondary font-semibold">
+      <div className="flex flex-col items-center justify-center py-6">
+         <h3 className="text-md flex items-center gap-2 md:text-xl text-secondary font-semibold">
+            <span>
+               <BiPlusMedical />
+            </span>
             {subtitle}
          </h3>
          <h2 className="text-3xl lg:text-4xl font-bold py-2">{title}</h2>
