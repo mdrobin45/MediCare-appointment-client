ad141f2d8dcc580934e2d3f5ed36d79506ae1dbb
Md Robin Rana <developer.smrobin45@gmail.com>
2023-12-02 20:12:42 +0600

    refactor code

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -1,12 +1,33 @@
 import PageHeader from "../../Components/PageHeader/PageHeader";
-import Book from "../../Containers/Booking/BookingContainer";
+import AppointmentForm from "./AppointmentForm/AppointmentForm";
+import useBookingLogic from "./ServiceLogic/useBookingLogic";
+import Summary from "./Summary/Summary";
 
 const Booking = () => {
+   const {
+      appointmentData,
+      setAppointmentData,
+      handleChange,
+      handleSubmit,
+      isPending,
+      totalPrice,
+   } = useBookingLogic();
    return (
-      <div>
+      <section>
          <PageHeader title="Book Appointment" />
-         <Book />
-      </div>
+         <div className="px-2 mt-10 sm:px-6 lg:px-8 flex justify-between gap-10">
+            <AppointmentForm
+               appointmentData={appointmentData}
+               setAppointmentData={setAppointmentData}
+               changeHandler={handleChange}
+            />
+            <Summary
+               isLoading={isPending}
+               handleSubmit={handleSubmit}
+               total={totalPrice}
+            />
+         </div>
+      </section>
    );
 };
 
